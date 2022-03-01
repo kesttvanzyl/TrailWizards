8c5150b293b7ec96d8055132a47ac779a1045927
Kestt van Zyl <54337592+kesttvanzyl@users.noreply.github.com>
2022-02-28 17:52:25 -0800

    Implemented Random Password Microservice

--- main.py
+++ main.py
@@ -5,7 +5,28 @@ import string
 import random
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.uic import loadUi
+import zmq
+from time import sleep
 
+""" A socket publisher and subscriber """
+# setting up a sending side to request a service
+# context variable
+context = zmq.Context()
+# setup the socket
+socket_snd = context.socket(zmq.PUB)
+# bind it to a socket, local this time, set to what ever you want to use
+socket_snd.bind('tcp://127.0.0.1:2001')  # local port for testing
+# socket.bind('tcp:////127.0.0.1:9523')     # run on the flip3 server with port 9523
+
+#  set up the socket to connect to the port for rec the reply
+socket_rec = context.socket(zmq.SUB)
+# connect the socket to the port (same as in the publisher)
+socket_rec.connect('tcp://127.0.0.1:2000')
+# socket.connect('tcp://127.0.0.1:9524')  # for use on the flip3 server
+# socket.connect('tcp://flip3.engr.oregonstate.edu:9524')  # experiment for listening locally
+
+# setting up the socket option and listen
+socket_rec.setsockopt_string(zmq.SUBSCRIBE, "")
 
 class LoginApp(QDialog):
     def __init__(self):
@@ -38,34 +59,31 @@ class RegisterApp(QDialog):
         passwordentry = self.passwordRegisterEntry.text()
 
     def generatepassword(self):
-        length = 15
-        upper = True
-        lower = True
-        digits = True
-        punctuation = True
-        white = False
 
-        chars = ''
+        while True:
+            message = "password()"
+            # quit the test program
+            if message == "quit":
+                return
+            # send the message to the TCP port
+            print("Going to send this message: ", message)
+            socket_snd.send_pyobj(message)
+            print('sent message:', message)
+            # setting up the received message
+            message_ret = socket_rec.recv_pyobj()
+            print("Returned message:", message_ret)
 
-        if upper:
-            chars = chars + string.ascii_uppercase
-        if lower:
-            chars = chars + string.ascii_lowercase
-        if digits:
-            chars = chars + string.digits
-        if punctuation:
-            chars = chars + string.punctuation
-        if white:
-            chars = chars + string.whitespace
-
-        res = ''.join(random.choice(chars) for i in range(0, length))
-
-        QMessageBox.information(self, "Password Generator Result", "Secure Password:  " + res + "\n Write down "
-                                                                                                "password and "
-                                                                                                "memorize it or keep "
-                                                                                                "it in a secure "
-                                                                                                "place.")
-
+            QMessageBox.information(self, "Password Generator Result", "Secure Password:  " + message_ret + "\n Write "
+                                                                                                            "down "
+                                                                                                            "password "
+                                                                                                            "and "
+                                                                                                            "memorize "
+                                                                                                            "it or "
+                                                                                                            "keep "
+                                                                                                            "it in a "
+                                                                                                            "secure "
+                                                                                                            "place.")
+            break
 
     def showloginpage(self):
         widget.setCurrentIndex(0)
@@ -75,6 +93,10 @@ class SchedulerApp(QDialog):
     def __init__(self):
         super(SchedulerApp, self).__init__()
         loadUi("SchedulerPageUI.ui", self)
+        self.logoutButton.clicked.connect(self.showloginpage)
+
+    def showloginpage(self):
+        widget.setCurrentIndex(0)
 
 
 app = QApplication(sys.argv)
@@ -89,6 +111,5 @@ widget.setCurrentIndex(0)
 widget.setFixedWidth(1100)
 widget.setFixedHeight(619)
 widget.show()
-
 
 app.exec_()
